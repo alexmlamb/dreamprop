56c3956bbee57d0b4fb6d9b4da2802ad0214beca
Alexander Lamb <lambalex@leto05.iro.umontreal.ca>
2017-02-13 15:16:34 -0500

    updates

--- lib/v2rnn_main.py
+++ lib/v2rnn_main.py
@@ -26,6 +26,11 @@ from viz import plot_images
 
 from load_cifar import CifarData
 
+srng = T.shared_randomstreams.RandomStreams(999)
+
+def dropout(inp, p=0.5):
+    return T.cast(srng.binomial(n=1,p=p,size=inp.shape),'float32') * inp
+
 def cast(inp):
     return T.cast(inp, 'float32')
 
@@ -42,15 +47,32 @@ def noneabove(rec_loss_lst,k,limit):
         if j <= k:
             return True
 
+
+class ConsiderConstant(theano.compile.ViewOp):
+    def grad(self, args, g_outs):
+        return [T.zeros_like(g_out) for g_out in g_outs]
+
+consider_constant = ConsiderConstant()
+
+
+
 dataset = "mnist"
 #dataset = "cifar"
 #dataset = "ptb_char"
 
 print "dataset", dataset
 
-do_synthmem = True
+do_synthmem = False
 
 print "do synthmem", do_synthmem
+
+do_forwardinc = False
+
+print "do forwardinc", do_forwardinc
+
+do_bptt = True
+
+print "do bptt", do_bptt
 
 sign_trick = False
 
@@ -60,7 +82,7 @@ use_class_loss_forward = 1.0
 
 print "use class loss forward", use_class_loss_forward
 
-only_y_last_step = False
+only_y_last_step = True
 
 print "only give y on last step", only_y_last_step
 
@@ -74,7 +96,7 @@ beta1_s = 0.9
 
 print "learning rate and beta synthmem_updates", lr_s, beta1_s
 
-num_steps = 1
+num_steps = 4
 
 limit = 200.0
 
@@ -141,10 +163,14 @@ def init_params_forward():
 
     tparams = init_tparams(p)
 
-    tparams['W1'] = theano.shared(0.03 * rng.normal(0,1,size=(1,m,m)).astype('float32'))
-    tparams['W2'] = theano.shared(0.03 * rng.normal(0,1,size=(1,m,m)).astype('float32'))
-    tparams['Wy'] = theano.shared(0.03 * rng.normal(0,1,size=(1,m,11)).astype('float32'))
+    #tparams['W1'] = theano.shared(0.03 * rng.normal(0,1,size=(1,m,m)).astype('float32'))
+    #tparams['W2'] = theano.shared(0.03 * rng.normal(0,1,size=(1,m,m)).astype('float32'))
+    tparams['Wy'] = theano.shared(0.03 * rng.normal(0,1,size=(m,11)).astype('float32'))
     tparams['W0'] = theano.shared(0.03 * rng.normal(0,1,size=(m+nf,m)).astype('float32'))
+
+    tparams['by'] = theano.shared(0.03 * rng.normal(0,1,size=(11,)).astype('float32'))
+    tparams['b0'] = theano.shared(0.03 * rng.normal(0,1,size=(m,)).astype('float32'))
+
 
     return tparams
 
@@ -152,26 +178,30 @@ def init_params_synthmem():
 
     pa = {}
 
-    param_init_lngru({}, params=pa, prefix='sm_gru1', nin=m, dim=m)
+    #param_init_lngru({}, params=pa, prefix='sm_gru1', nin=m, dim=m)
     
     p = init_tparams(pa)
 
-    p['Wh'] = theano.shared(0.03 * rng.normal(0,1,size=(m,m)).astype('float32'))
-    #p['Wh2'] = theano.shared(0.03 * rng.normal(0,1,size=(1,1024,1024)).astype('float32'))
+    smu = 1024
 
-    p['Wx'] = theano.shared(0.03 * rng.normal(0,1,size=(1,m,m)).astype('float32'))
-    p['Wx2'] = theano.shared(0.03 * rng.normal(0,1,size=(1,m,nf)).astype('float32'))
+    print "number of synthmem units", smu
 
-    p['Wy1'] = theano.shared(0.03 * rng.normal(0,1,size=(1,m,m)).astype('float32'))
-    p['Wy2'] = theano.shared(0.03 * rng.normal(0,1,size=(1,m,11)).astype('float32'))
+    p['Wh'] = theano.shared(0.03 * rng.normal(0,1,size=(m,smu)).astype('float32'))
+    p['Wh2'] = theano.shared(0.03 * rng.normal(0,1,size=(smu,m)).astype('float32'))
 
-    p['bh'] = theano.shared(0.03 * rng.normal(0,1,size=(m,)).astype('float32'))
-    #p['bh2'] = theano.shared(0.03 * rng.normal(0,1,size=(1,1024,)).astype('float32'))
+    p['Wx'] = theano.shared(0.03 * rng.normal(0,1,size=(m,m)).astype('float32'))
+    p['Wx2'] = theano.shared(0.03 * rng.normal(0,1,size=(m,nf)).astype('float32'))
+
+    p['Wy1'] = theano.shared(0.03 * rng.normal(0,1,size=(m,m)).astype('float32'))
+    p['Wy2'] = theano.shared(0.03 * rng.normal(0,1,size=(m,11)).astype('float32'))
+
+    p['bh'] = theano.shared(0.03 * rng.normal(0,1,size=(smu,)).astype('float32'))
+    p['bh2'] = theano.shared(0.03 * rng.normal(0,1,size=(m,)).astype('float32'))
     
-    p['bx'] = theano.shared(0.03 * rng.normal(0,1,size=(1,m,)).astype('float32'))
-    p['bx2'] = theano.shared(0.03 * rng.normal(0,1,size=(1,nf,)).astype('float32'))
+    p['bx'] = theano.shared(0.03 * rng.normal(0,1,size=(m,)).astype('float32'))
+    p['bx2'] = theano.shared(0.03 * rng.normal(0,1,size=(nf,)).astype('float32'))
 
-    p['by1'] = theano.shared(0.03 * rng.normal(0,1,size=(1,m,)).astype('float32'))
+    p['by1'] = theano.shared(0.03 * rng.normal(0,1,size=(m,)).astype('float32'))
 
     return p
 
@@ -179,9 +209,12 @@ def init_params_synthmem():
 def join2(a,b):
         return T.concatenate([a,b], axis = 1)
 
-print "USING FULL LN"
+print "NOT USING FULL LN 1.001"
 def ln(inp):
-    return (inp - T.mean(inp,axis=1,keepdims=True)) / (0.001 + T.std(inp,axis=1,keepdims=True))
+    return inp# - T.mean(inp,axis=1,keepdims=True)) / (1.001 + T.std(inp,axis=1,keepdims=True))
+
+def ln2(inp):
+    return (inp - T.mean(inp,axis=1,keepdims=True)) / (1.001 + T.std(inp,axis=1,keepdims=True))
 
 def forward(p, h, x_true, y_true, i):
 
@@ -189,18 +222,20 @@ def forward(p, h, x_true, y_true, i):
 
     inp = join2(h, x_true)
 
-    emb = T.dot(inp, p['W0'])
+    emb = T.dot(inp, p['W0']) + p['b0']
 
     h0 = lngru_layer(p,emb,{},prefix='gru1',mask=None,one_step=True,init_state=h[:,:m],backwards=False)
 
-    h1 = T.nnet.relu(ln(T.dot(h0[0], p['W1'][i])),alpha=0.02)
-    h2 = T.nnet.relu(ln(T.dot(h1, p['W2'][i])),alpha=0.02)
+    #h1 = T.nnet.relu(ln(T.dot(h0[0], p['W1'][i])),alpha=0.02)
+    #h2 = T.nnet.relu(ln(T.dot(h1, p['W2'][i])),alpha=0.02)
     #h2 = h1
 
-    y_est = T.nnet.softmax(T.dot(h2, p['Wy'][i]))
 
-    #h_next = T.dot(h2, p['Wo'][i])
-    h_next = h1
+    h2 = h0[0]
+
+    y_est = T.nnet.softmax(T.dot(h2, p['Wy']) + p['by'])
+
+    h_next = h0[0]
 
     loss = crossent(y_est, y_true)
 
@@ -211,21 +246,23 @@ def forward(p, h, x_true, y_true, i):
 def synthmem(p, h_next, i): 
 
     i *= 0
+
+    h_next = dropout(h_next,0.1)
+
+    print "USING DROPOUT IN SYNTHMEM"
+
+    emb = T.nnet.relu(T.dot(h_next, p['Wh']) + p['bh'], alpha=0.02)
+    h2 = T.dot(emb, p['Wh2']) + p['bh2']
+
+    #hn2 = lngru_layer(p,emb,{},prefix='sm_gru1',mask=None,one_step=True,init_state=h_next[:,:m],backwards=False)
+
+    xh1 = T.nnet.relu(ln(T.dot(h_next, p['Wx']) + p['bx']), alpha=0.02)
+    x = T.dot(xh1, p['Wx2']) + p['bx2']
     
-    emb = T.dot(h_next, p['Wh']) + p['bh']
+    yh1 = T.nnet.relu(ln(T.dot(h_next, p['Wy1']) + p['by1']), alpha=0.02)
+    y = T.nnet.softmax(T.dot(yh1, p['Wy2']))
 
-    hn2 = lngru_layer(p,emb,{},prefix='sm_gru1',mask=None,one_step=True,init_state=h_next[:,:m],backwards=False)
-    #hn1 = T.tanh(T.dot(h_next, p['Wh'][i]) + p['bh'][i])
-    #hn2 = T.tanh(T.dot(hn1[0], p['Wh2'][i]) + p['bh2'][i])
-
-    xh1 = T.nnet.relu(ln(T.dot(h_next, p['Wx'][i]) + p['bx'][i]), alpha=0.02)
-    x = T.dot(xh1, p['Wx2'][i]) + p['bx2'][i]
-    
-    yh1 = T.nnet.relu(ln(T.dot(h_next, p['Wy1'][i]) + p['by1'][i]), alpha=0.02)
-    y = T.nnet.softmax(T.dot(yh1, p['Wy2'][i]))
-
-    return hn2[0], x, y
-
+    return h2, x, y
 
 params_forward = init_params_forward()
 params_synthmem = init_params_synthmem()
@@ -310,14 +347,48 @@ synthmem_method = theano.function(inputs = [h_next, g_next, step], outputs = [h_
 ##########################################################################
 
 Xts = T.tensor3()
-Yts = T.imatrix()
+y = T.ivector()
 
-train_bptt = bptt_func()
+h_initial = theano.shared(np.zeros((64,m)).astype('float32'))
+h_last = h_initial
+
+bptt_lr = 0.001
+bptt_beta1 = 0.9
+
+print "bptt params", bptt_lr, bptt_beta1
+
+hrw = 0.001
+
+print "hrw", hrw
+
+total_loss = 0.0
+
+def dist(a,b):
+    return T.sum(T.sqr(a-b))
+
+for step in range(0, num_steps):
+    h_next_rec, y_est, class_loss,acc,probs = forward(params_forward, h_last, Xts[step], y,step)
+    h_rec,x_rec,y_rec = synthmem(params_synthmem, h_next_rec, step)
+
+    print "ONLY RECONSTRUCTING H/X with gradient blocking"
+    h_rec_loss = hrw * (1.0 * dist(consider_constant(h_last), h_rec) + 1.0 * dist(Xts[step], x_rec) + 0.0 * dist(expand(y,11), y_rec))
+
+    h_last = h_next_rec
+    last_acc = acc
+    total_loss += class_loss + h_rec_loss
+
+bptt_updates = lasagne.updates.rmsprop(total_loss, params_forward.values() + params_synthmem.values(), learning_rate=bptt_lr)
+
+bptt_train = theano.function([Xts, y], outputs = [last_acc,total_loss], updates=bptt_updates)
+
+########################################################################
+#Main loop section
+########################################################################
 
 h_forward_lst = [0]*num_steps
 rec_loss_lst = [0]*num_steps
 
-for iteration in xrange(0,100000):
+for iteration in xrange(0,200000):
     r = randint(0,49900)
 
     x = trainx[r:r+64]
@@ -329,14 +400,17 @@ for iteration in xrange(0,100000):
     h_final = h_forward_lst[-1]
 
     for (j,k) in zip(range(num_steps),reversed(range(num_steps))):
-        x_step = x[:,j*nf:(j+1)*nf]
-        h_next, rec_loss, class_loss,acc,y_est = forward_method(x_step,y,h_in,j)
-        h_forward_lst[j] = h_next
-        h_in = h_next
-        rec_loss_lst[j] = rec_loss
 
-        if iteration % 100 == 0:
-            print "rec loss", j, rec_loss
+        if do_forwardinc:
+
+            x_step = x[:,j*nf:(j+1)*nf]
+            h_next, rec_loss, class_loss,acc,y_est = forward_method(x_step,y,h_in,j)
+            h_forward_lst[j] = h_next
+            h_in = h_next
+            rec_loss_lst[j] = rec_loss
+
+            if iteration % 100 == 0:
+                print "rec loss", j, rec_loss
 
         if do_synthmem and iteration > 1000 and noneabove(rec_loss_lst,k,limit):
             if iteration % 100 == 1:
@@ -345,14 +419,36 @@ for iteration in xrange(0,100000):
             g_next = g_last
             h_final = h_next_synthmem
 
+
+
+    #########
+    #BPTT Method
+    #########
+
+    if do_bptt:
+
+        xsteplst = []
+
+        for j in range(0,num_steps):
+            xsteplst.append(x[:,j*nf:(j+1)*nf])
+
+        xts = np.asarray(xsteplst)
+
+
+        last_acc, total_loss = bptt_train(xts,y)
+
+
     #using 500
     if iteration % 100 == 0:
         
-
+        print "Total Loss Train bptt", total_loss
+        print "Acc Train bptt", last_acc
         print "========================================"
-        print "train acc", acc
-        print "train cost", class_loss
-        print "train rec_loss", rec_loss
+
+        if do_forwardinc:
+            print "train acc", acc
+            print "train cost", class_loss
+            print "train rec_loss", rec_loss
         va = []
         vc = []
         for ind in range(0,10000,1000):
